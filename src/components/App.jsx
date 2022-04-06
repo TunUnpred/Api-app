d83410ae346eeca974f3feadf6e8226beebcbd08
khaled <sahlikhaled25@gmail.com>
2022-04-06 12:26:59 +0100

    Api initial app

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Principal from './Principal';
+
+function App() {
+    return (
+        <div>
+            <Principal />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Principal.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { Domino } from '../images/domino.png';
+
+
+function Principal() {
+    return (
+        <div>
+            <div className='bg'>
+                <div className='screen'>
+                    <h3>ADVICE</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <div className='glow-on-hover'>
+                        {/* <img src={Domino} alt='img' className='img'></img> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Principal;
